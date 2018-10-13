b69f7a5b665774509739c881a462469ef2224def
Samko Yoon <samko.yoon@servian.com>
2018-10-14 02:43:19 +1100

    ee

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const
     bodyParser = require("body-parser"),
     app = express(),
     port = 3002;
-
+const Credstash = require('credstash');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+	console.log('ENV ::: ', process.env);
 app.get('/', (req, res) => res.send("I am healthy~~~"));
 app.get('/env', (req, res) => {
+	console.log('ENV ::: ', process.env);
     res.send('Env value is ' + process.env.PROFILE+' and start with'+process.env.START+', '+process.env.TEST+', '+process.env.TEST2);
 })
+var credstash = new Credstash({ 'table': 'credential-store', 'awsOpts' : { 'region': 'ap-southeast-2' }, 'kmsOpts': { 'region' : 'ap-southeast-2'}} )
+credstash.get('test.var', (e, secret) => {
+  console.log('do not share the secret', secret);
+});
 console.log('Env value is ' + process.env.PROFILE);
 app.listen(port, () => console.log(`Listening on ${port}`));
 
--- start.sh
+++ start.sh
@@ -3,9 +3,13 @@ echo 'running on '$PROFILE
 if [ "$PROFILE" = "dev" ]
 then 
 	echo 'this is dev'
-	export START=$(credstash get test.var)
+	export START=$(credstash -r 'ap-southeast-2' get test.var)
+	echo 'start2 '$START
 else 
 	echo 'this is not dev'
+		export START=$(credstash -r 'ap-southeast-2' get test.var)
+		export PROFILE=$START
+	echo 'start2 '$START
 fi
 #credstash -r ap-southeast-2 setup
 #export DB_URL=$(credstash -r ap-southeast-2 get DB_URL)
